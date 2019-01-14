e94e4d3702b749e61e6cd6c260c502580a217937
Rahul Mohan <mohanrahul93@gmail.com>
2019-01-14 18:30:43 +0530

    last number of fib completed and added

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -16,11 +16,25 @@ public class FibonacciLastDigit {
 
         return current % 10;
     }
+    private static int getFibonacciLastDigit(int n){
+    	if(n<=1)
+    		return n;
+
+    	int previous =0;
+    	int current =1;
+
+    	for(int i=0; i<n-1; i++){
+    		int temppre = previous;
+    		previous = current;
+    		current=(previous+temppre)%10;
+    	}
+    	return current;
+    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        int c = getFibonacciLastDigit(n);
         System.out.println(c);
     }
 }
