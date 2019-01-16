61a8fd4e276513a231a21e44c85953347bd1ce10
Rahul Mohan <mohanrahul93@gmail.com>
2019-01-16 14:56:53 +0530

    LCM of week two completed in java

--- week2_algorithmic_warmup/4_least_common_multiple/LCM.java
+++ week2_algorithmic_warmup/4_least_common_multiple/LCM.java
@@ -8,12 +8,23 @@ public class LCM {
 
     return (long) a * b;
   }
+  private static int gcd(int a,int b){
+      if (b==0){
+        return a;
+      }
+      else{
+        int remander = a%b;
+        return gcd(b,remander);
+      }
+    }
+
 
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
-
-    System.out.println(lcm_naive(a, b));
+    long gcdNum=gcd(a,b);
+    long answer=(long)a*b/gcdNum;
+    System.out.print(answer);
   }
 }
